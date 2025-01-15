f0a23ae7e1f09bc48a50550c27224d6db65ee593
Hafiz <23043274@myrp.edu.sg>
2025-01-15 23:58:02 +0800

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
-import About from "./About";
 import Categories from "./Categories";
 import Category from "./Category";
+import Session from "./Session";
+import Register from "./Register";
+import Confirmation from "./Confirmation";
 import Header from "./Header";
 
 function App() {
@@ -12,11 +14,15 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
-        <Route path="about" element={<About />} />
+        <Route path="/" element={<Home title="Home" />} />
         <Route path="categories" element={<Categories />}>
-          <Route path=":catId" element={<Category />} />
+          <Route path=":catId" element={<Category />} > 
+            <Route path=":sessionId" element={<Session />} />
+          </Route>
+          <Route index element={<h3>Select a category from above</h3>}/>
         </Route>
+        <Route path="register" element={<Register/>} />
+        <Route path="confirmed" element={<Confirmation />} />
         <Route
           path="*"
           element={<h1 className="not-found">Page Not Found</h1>}
